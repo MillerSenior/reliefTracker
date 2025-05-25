7185715fed199d3ffa3f9e34242374e588b69984
MillerSenior <anthony.millersr82@gmail.com>
2025-05-25 16:47:25 -0500

    production icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: siteConfig.name,
   description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
